219c62f8ad111a66634b0381101aa1b871d73f5c
bwarsaw <>
2004-02-10 22:46:52 +0000

    Bump the version number to 2.1.5a3.  Also bump the PENDING_FILE_SCHEMA_VERSION to indicate that we're now saving things in a per-list database.

--- Mailman/Version.py
+++ Mailman/Version.py
@@ -15,7 +15,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 # Mailman version
-VERSION = "2.1.5a1"
+VERSION = "2.1.5a3"
 
 # And as a hex number in the manner of PY_VERSION_HEX
 ALPHA = 0xa
@@ -30,7 +30,7 @@ MINOR_REV = 1
 MICRO_REV = 5
 REL_LEVEL = ALPHA
 # at most 15 beta releases!
-REL_SERIAL = 1
+REL_SERIAL = 3
 
 HEX_VERSION = ((MAJOR_REV << 24) | (MINOR_REV << 16) | (MICRO_REV << 8) |
                (REL_LEVEL << 4)  | (REL_SERIAL << 0))
@@ -41,8 +41,8 @@ DATA_FILE_VERSION = 90
 # qfile/*.db schema version number
 QFILE_SCHEMA_VERSION = 3
 
-# version number for the data/pending.db file schema
-PENDING_FILE_SCHEMA_VERSION = 1
+# version number for the lists/<listname>/pending.db file schema
+PENDING_FILE_SCHEMA_VERSION = 2
 
 # version number for the lists/<listname>/request.db file schema
 REQUESTS_FILE_SCHEMA_VERSION = 1
