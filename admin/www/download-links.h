3eaec661b868ed833fb82be13a15383645eb1601
bwarsaw <>
2006-09-13 03:21:03 +0000

    Tag as 2.1.9 (final).  Also, remove version.ht* since it's redundant.

--- admin/www/download-links.h
+++ admin/www/download-links.h
@@ -3,4 +3,3 @@
 <li><a href="requirements.html">Requirements</a>
 <li><a href="download.html">Downloading</a>
 <li><a href="install.html">Installing</a>
-<li><a href="version.html">Latest Version</a>
