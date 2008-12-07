2274e704c435716f33ed9dabcb80bef0811ba50c
Mark Sapiro <mark@msapiro.net>
2008-12-07 09:41:12 -0800

    Fixed a problem in Decorate which could throw a TypeError on conversion to unicode of a header/footer that was already unicode because of interpolating a unicode value.
    Made a minor change to the authentication logic for displaying hidden
    members on the roster.

--- Mailman/Handlers/Decorate.py
+++ Mailman/Handlers/Decorate.py
@@ -98,8 +98,16 @@ def process(mlist, msg, msgdata):
         # TK: Try to keep the message plain by converting the header/
         # footer/oldpayload into unicode and encode with mcset/lcset.
         # Try to decode qp/base64 also.
-        uheader = unicode(header, lcset, 'ignore')
-        ufooter = unicode(footer, lcset, 'ignore')
+        # It is possible header/footer is already unicode if it was
+        # interpolated with a unicode.
+        if isinstance(header, unicode):
+            uheader = header
+        else:
+            uheader = unicode(header, lcset, 'ignore')
+        if isinstance(footer, unicode):
+            ufooter = footer
+        else:
+            ufooter = unicode(footer, lcset, 'ignore')
         try:
             oldpayload = unicode(msg.get_payload(decode=True), mcset)
             frontsep = endsep = u''
