36f6592662f0a84b60b2277a00cf3cc85df52b8d
Mark Sapiro <mark@msapiro.net>
2013-07-12 14:29:44 -0700

    - Added logging for template errors in HyperArch.py.  (LP: #558254)

--- Mailman/Archiver/HyperArch.py
+++ Mailman/Archiver/HyperArch.py
@@ -226,9 +226,9 @@ def quick_maketext(templatefile, dict=None, lang=None, mlist=None):
                                     Utils.GetCharSet(lang),
                                     'replace')
                 text = sdict.interpolate(utemplate)
-        except (TypeError, ValueError):
+        except (TypeError, ValueError), e:
             # The template is really screwed up
-            pass
+            syslog('error', 'broken template: %s\n%s', filepath, e)
     # Make sure the text is in the given character set, or html-ify any bogus
     # characters.
     return Utils.uncanonstr(text, lang)
