4086aa6244b4a50ab7ed654876add30d801b30de
Mark Sapiro <mark@msapiro.net>
2013-07-14 02:10:07 -0700

    Bumped version and updated docs for 2.1.16rc1.

--- doc/mailman-member/labels.pl
+++ doc/mailman-member/labels.pl
@@ -1,4 +1,4 @@
-# LaTeX2HTML 2002-2-1 (1.71)
+# LaTeX2HTML 2008 (1.71)
 # Associate labels original text with physical files.
 
 
@@ -34,16 +34,16 @@ $key = q/sec:nomail/;
 $external_labels{$key} = "$URL/" . q|node20.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/sec:nodupes/;
+$external_labels{$key} = "$URL/" . q|node21.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/sec:sometopic/;
 $external_labels{$key} = "$URL/" . q|node31.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:MIME/;
 $external_labels{$key} = "$URL/" . q|node28.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:nodupes/;
-$external_labels{$key} = "$URL/" . q|node21.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:nolist/;
@@ -113,7 +113,7 @@ $noresave{$key} = "$nosave";
 1;
 
 
-# LaTeX2HTML 2002-2-1 (1.71)
+# LaTeX2HTML 2008 (1.71)
 # labels from external_latex_labels array.
 
 
